d0947810d8d719a00ba811c08f40a0beac5d1faa
Lautaro Chaile <lauty1990@gmail.com>
2020-08-31 22:08:24 -0300

    feat(frontend): add drawer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import '../assets/css/App.css'
+import '../assets/css/App.css';
 
-// Export components
 import Layout from './Layout';
-import NavBar from './NavBar';
 
 function App() {
   return (
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import '../assets/css/Layout.css'
-//Imports Material Ui
+import '../assets/css/Layout.css';
 
-//Export Components
-import NavBar from './NavBar';
-
+import PersistentDrawerLeft from './Drawer';
 
 function Layout() {
-    return (
-        <div>
-            <NavBar></NavBar>
-        </div>
-    )
-  }
+  return (
+    <div>
+      <PersistentDrawerLeft></PersistentDrawerLeft>
+    </div>
+  );
+}
 
 export default Layout;
