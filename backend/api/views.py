34c84245fef1400da44c6ebaeea6e63ae5152400
Ignacio Martinez <itsigna02@gmail.com>
2020-09-17 14:12:06 -0300

    feat(backend): add views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course, Student, Attendance, Parent, Allergy, Diet
+from .serializers import CourseSerializer, StudentSerializer, AttendanceSerializer, ParentSerializer, AllergySerializer, DietSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+
+
+class ParentViewSet(viewsets.ModelViewSet):
+    queryset = Parent.objects.all()
+    serializer_class = ParentSerializer
+
+
+class AllergyViewSet(viewsets.ModelViewSet):
+    queryset = Allergy.objects.all()
+    serializer_class = AllergySerializer
+
+
+class DietViewSet(viewsets.ModelViewSet):
+    queryset = Diet.objects.all()
+    serializer_class = DietSerializer
